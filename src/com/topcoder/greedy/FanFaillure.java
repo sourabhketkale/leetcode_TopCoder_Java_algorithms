789640fc07582c69e6b018d7d7f7344b0efde25f
sourabhketkale <ketkalesourabh129@gmail.com>
2016-11-07 13:06:36 -0800

    k2: Fan failure problem. Greedy algorithms Top coder

---
+++ src/com/topcoder/greedy/FanFaillure.java
@@ -0,0 +1,48 @@
+package com.topcoder.greedy;
+
+/**
+ * Problem Statement:
+ * 
+ * 
+ * 
+ * Test Cases:
+ * 
+ * 0)	
+    	
+	{1,2,3}
+	2
+	Returns: { 2,  1 }
+	Example from the problem statement.
+	1)	
+	    	
+	{8,5,6,7}
+	22
+	Returns: { 0,  0 }
+	No fans can fail in this system.
+	2)	
+	    	
+	{676, 11, 223, 413, 823, 122, 547, 187, 28}
+	1000
+	Returns: { 7,  2 }
+	If you eliminate fans with 676, 11, 413, 122, 547, 187, and 28, you are left with 223 + 823 = 1046 
+	units of cooling, which is sufficient, yielding an MFS size of 7. If you eliminate 676, 823, and 547, 
+	you are left with only 984 units of cooling. All combinations of 2 or less fans breaking leaves sufficient 
+	cooling, so the MFC is 2.
+ * 
+ * */
+
+public class FanFaillure {
+	
+	int[] getRange(int[] capacities, int minCooling){
+		
+			
+		
+		return  null;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
