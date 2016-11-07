e5347e00573def786a5f54e312bca89dee9254e7
sourabhketkale <ketkalesourabh129@gmail.com>
2016-11-07 13:51:54 -0800

    k2: Scratching the surface. Queue using 2 stacks using generics.

---
+++ src/com/algorithms/StacksAndQueues/QueueUsing2StacksGenerics.java
@@ -0,0 +1,62 @@
+package com.algorithms.StacksAndQueues;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class QueueUsing2StacksGenerics {
+    public static class MyQueue<T> {
+        Stack<T> stackNewestOnTop = new Stack<T>();
+        Stack<T> stackOldestOnTop = new Stack<T>();
+
+        public void enqueue(T value) { // Push onto newest stack
+                        
+            stackOldestOnTop.push(value);
+        }
+
+        public T peek() {
+            if(stackNewestOnTop.isEmpty() && !stackOldestOnTop.isEmpty()){
+                while(!stackOldestOnTop.isEmpty()){
+                 stackNewestOnTop.push(stackOldestOnTop.pop());
+             }
+            }else if(!stackNewestOnTop.isEmpty()){
+                return stackNewestOnTop.peek();
+            }else{
+                return null;
+            }
+          
+            return stackNewestOnTop.peek();
+        }
+
+        public T dequeue() {
+            if(stackOldestOnTop.isEmpty()){
+                return null;
+            } 
+            while(!stackOldestOnTop.isEmpty()){
+                 stackNewestOnTop.push(stackOldestOnTop.pop());
+             }
+            return stackNewestOnTop.pop();
+        }
+    }
+
+    
+    public static void main(String[] args) {
+        MyQueue<Integer> queue = new MyQueue<Integer>();
+        
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        
+        for (int i = 0; i < n; i++) {
+            int operation = scan.nextInt();
+            if (operation == 1) { // enqueue
+                queue.enqueue(scan.nextInt());
+            } else if (operation == 2) { // dequeue
+                queue.dequeue();
+            } else if (operation == 3) { // print/peek
+                System.out.println(queue.peek());
+            }
+        }
+        scan.close();
+    }
+}
