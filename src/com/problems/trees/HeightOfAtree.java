893ffb44e900b916478da25a241ea0bb6420bbc3
sourabhketkale <ketkalesourabh129@gmail.com>
2016-11-10 11:03:18 -0800

    k2: Added scanner and insert. #boilerPlate

--- src/com/problems/trees/HeightOfAtree.java
+++ src/com/problems/trees/HeightOfAtree.java
@@ -2,6 +2,7 @@ package com.problems.trees;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 /**
  * @Method: Iterative method to calculate binary tree using Queue handling all corner cases.
@@ -63,5 +64,36 @@ public class HeightOfAtree {
      
      return height-1;
  }
-
+	
+	public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node();
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            int T=sc.nextInt();
+            Node root=null;
+            while(T-->0){
+                int data=sc.nextInt();
+                root=insert(root,data);
+            }
+            int height=getHeight(root);
+            System.out.println(height);
+        }	
 }
+	
+
+
