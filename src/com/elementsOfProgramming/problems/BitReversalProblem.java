160efcff9f1ebd5f2844e170bd7e4a668c3badd7
sourabhketkale <ketkalesourabh129@gmail.com>
2016-11-18 13:15:13 -0800

    k2: Clear bit method to clear  a kth bit.

--- src/com/elementsOfProgramming/problems/BitReversalProblem.java
+++ src/com/elementsOfProgramming/problems/BitReversalProblem.java
@@ -8,6 +8,7 @@ package com.elementsOfProgramming.problems;
  * 	5.1: Calculate parity of the 64 bit number.
  * 	5.3: Write a function that takes a 64-bit integer x and returns a 64-bit integer
                 consisting of the bits of x in reverse order. #Elements of programming
+        : Clear bit method to flip a given bit in a number
  * 	Playing around with bits. Knowing the Combinations of ~,^,&,| to be put to use
  * 
  * @input: int input 32 bit number  
@@ -34,18 +35,28 @@ public class BitReversalProblem {
 		  }
 		  return y;
 		}
+	
+	/**
+	 * @method: Clear bit method to flip the kth bit in a number
+	 * Logic: Make  a bit sequencce in which only kth bit is set and then AND the number with the clear bit number
+	 * */ 
+	public static int clearBit(int x, int kth) {
+		  return (x & ~(1 << kth));
+		}
 		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		long input= 32;
 
-	    int a = 39;	/* 60 = 0011 1100 */
+	    int a =39;	/* 60 = 0011 1100 */
 	    int b = 15;	/* 13 = 0000 1101 */
 	    int c = 0;
 	    //System.out.println(BitReversalProblem.reverseBitsInt(a));
 	    
-	    System.out.println(BitReversalProblem.reverseBitsInt(a));
+	    //System.out.println(BitReversalProblem.reverseBitsInt(a));
+	    System.out.println(Integer.toBinaryString(a)); 
+	    System.out.println(Integer.toBinaryString(BitReversalProblem.clearBit(a, 0)));
 	    
 	    /* System.out.println(Integer.toBinaryString(a));
 	    System.out.println(BitReversalProblem.reverseBitsInt(a));
